eaaae43ef6bbc9638903833076dfdd36477fe38f
mmaduabuchi <emmanuelukwe1@gmail.com>
2025-08-30 19:21:58 +0100

    added to the personal account

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -7,25 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Balance extends Model
 {
 
-<<<<<<< HEAD
-protected $fillable = ['user_id', 'name', 'currency', 'amount'];
-
-
-  public function user()
-=======
 protected $fillable = ['user_id', 'personal_id', 'name', 'currency', 'amount'];
 
 public function user()
->>>>>>> 4214aa702807e3d23954c2ccb8c80301d29082d1
 {
     return $this->belongsTo(User::class);
 }
 
-<<<<<<< HEAD
-=======
     public function personal()
     {
         return $this->belongsTo(Personal::class);
     }
->>>>>>> 4214aa702807e3d23954c2ccb8c80301d29082d1
 }
