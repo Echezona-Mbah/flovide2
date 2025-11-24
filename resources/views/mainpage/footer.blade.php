d2490ecae2abe01e4e312b4f6e447c1cd1457cee
mmaduabuchi <emmanuelukwe1@gmail.com>
2025-11-24 15:53:44 +0100

    added career page and page translation

--- resources/views/mainpage/footer.blade.php
+++ resources/views/mainpage/footer.blade.php
@@ -100,3 +100,19 @@
     </div>
   </section>
 </footer>
+
+
+
+<script type="text/javascript">
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+      {
+        pageLanguage: 'en',
+        includedLanguages: 'en,es,fr,lg'
+      },
+      'google_translate_element'
+    );
+  }
+</script>
+
+<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
