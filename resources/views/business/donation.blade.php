4378014c75ec65f2341ca58e8aa71fc0f32852d2
mmaduabuchi <emmanuelukwe1@gmail.com>
2025-12-19 11:25:09 +0100

    added records count to donation,remita,payment pages and did somelittle changes on thier respective controllers

--- resources/views/business/donation.blade.php
+++ resources/views/business/donation.blade.php
@@ -58,7 +58,7 @@
                             @endif
                         </script>
                         <!-- Cards Grid -->
-                        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-20">
+                        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-20">
                             
                             @foreach ($donations as $donation )
                                 <!-- Card 1 -->
@@ -89,7 +89,7 @@
                                         <div class="flex gap-4 items-center">
                                             <div class="flex items-center gap-1">
                                                 <img src=" {{ asset("asserts/dashboard/person.png") }}" alt="">
-                                                {{ $payment_count[$donation->id] ?? 0  }}
+                                                {{ $donation->records_count ?? 0  }}
                                             </div>
                                             <div class="bg-[#F9F7E5] {{ ucfirst($donation->visibility) == "Private" ? 'bg-[#F9F7E5]' : 'bg-[#d1f0d9]' }}  text-[#2f6a44] rounded-full w-7 h-7 flex items-center justify-center">
                                                 @if ( ucfirst($donation->visibility) == 'Private')
@@ -116,42 +116,7 @@
                             
                             @endforeach
                             
-
                             
-                            <div class="bg-white rounded-2xl p-4 flex flex-col justify-between shadow-sm gap-y-4 border border-[#D6D6D6]">
-                                <div class="flex justify-between items-center gap-4">
-                                    <div class="w-10 h-10 rounded-lg border border-gray-300 flex items-center justify-center text-gray-400">
-                                        <img src="asserts/dashboard/copy.png" alt="">
-                                    </div>
-                                    <div>
-                                        <div class="flex justify-between items-start">
-                                            <div class="text-lg font-extrabold">5 GBP</div>
-                                        </div>
-                                        {{-- <div class="text-xs bg-gray-300 text-gray-700 rounded-full px-2 py-1 inline-block mt-1 select-none">Monthly</div> --}}
-                                    </div>
-                                </div>
-                            
-                                <h3 class="font-semibold text-md leading-tight">Using Storytelling to Engage Food Deli...</h3>
-                                <div class="flex items-center justify-between gap-3 mt-4 text-sm font-semibold text-gray-700">
-                                    <div class="flex gap-4 items-center">
-                                        <div class="flex items-center gap-1">
-                                            <img src=" {{ asset("asserts/dashboard/person.png") }}" alt="">
-                                            50
-                                        </div>
-                                        <div class="bg-[#d1f0d9] text-[#2f6a44] rounded-full w-7 h-7 flex items-center justify-center">
-                                            <i class="fas fa-check text-[10px]"></i>
-                                        </div>
-                                    </div>
-                                    <div class="flex gap-4">
-                                        <button aria-label="Link button" class="bg-gray-200 rounded-full w-8 h-8 flex items-center justify-center text-gray-600 hover:bg-gray-300 transition">
-                                            <i class="fas fa-link"></i>
-                                        </button>
-                                        <button aria-label="Next button" class="bg-gray-200 rounded-full w-8 h-8 flex items-center justify-center text-gray-600 hover:bg-gray-300 transition">
-                                            <i class="fas fa-chevron-right"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
 
                     </div>
