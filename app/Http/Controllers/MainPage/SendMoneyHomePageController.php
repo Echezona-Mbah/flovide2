56b454ba14a8b2a61b45ba4764cbae9f43dc3da6
Echezona-Mbah <cclassico998@gmail.com>
2026-02-07 14:02:59 +0100

    Update affiliate page

--- app/Http/Controllers/MainPage/SendMoneyHomePageController.php
+++ app/Http/Controllers/MainPage/SendMoneyHomePageController.php
@@ -16,10 +16,8 @@ class SendMoneyHomePageController extends Controller
 
 public function index($slug = null)
 {
-    // Fetch all exchange rates from DB
     $exchangeRates = ExchangeRate::all();
 
-    // Build currencies array from database
     $currencies = $exchangeRates->mapWithKeys(function ($rate) {
 
         $countrySlug = Str::slug($rate->country_name);
@@ -35,33 +33,26 @@ public function index($slug = null)
         ];
     });
 
-    // 🔍 Find selected country from slug
-    $countryCurrency = null;
-
+    // Default = first currency
+    $countryCurrency = $currencies->first(); // default
     if ($slug) {
-
         if (!str_starts_with($slug, 'send-money-to-')) {
             abort(404);
         }
 
         $countrySlug = str_replace('send-money-to-', '', $slug);
 
-        foreach ($currencies as $data) {
+        foreach ($currencies as $code => $data) {
             if ($data['country'] === $countrySlug) {
                 $countryCurrency = $data;
+                $countryCurrency['currency_code'] = $code; // <-- add this
                 break;
             }
         }
-
-        if (!$countryCurrency) {
-            abort(404);
-        }
     }
 
-    return view('mainpage.send-money', [
-        'currencies' => $currencies,
-        'countryCurrency' => $countryCurrency
-    ]);
+
+    return view('mainpage.send-money', compact('currencies', 'countryCurrency'));
 }
 
 
