85ffb49c96f7d0d9a07e70a8ba255392d9863393
Echezona-Mbah <cclassico998@gmail.com>
2026-04-23 17:28:29 +0100

    remove secret and cleanup resources

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -58,12 +58,21 @@ public function registerUser(Request $request)
         'trading_city' => 'required|string|max:255',
     ]);
 
-    return response()->json([
-        'success' => false,
-        'message' => 'Validation error',
-        'code' => 'VALIDATION_ERROR',
-        'data' => $validator->errors()
-    ], 422);
+    // return response()->json([
+    //     'success' => false,
+    //     'message' => 'Validation error',
+    //     'code' => 'VALIDATION_ERROR',
+    //     'data' => $validator->errors()
+    // ], 422);
+
+      if ($validator->fails()) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Validation error',
+            'code' => 'VALIDATION_ERROR',
+            'data' => $validator->errors()
+        ], 422);
+    }
 
     if (
         User::where('email', $request->email)->exists() ||
